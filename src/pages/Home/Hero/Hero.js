b34cb422aff1c173708492c40734012fd9a3bcf3
Pradeep Gupta <pradeep@zerobill.app>
2023-10-21 09:03:49 +0530

    Home Screen Changes

--- src/pages/Home/Hero/Hero.js
+++ src/pages/Home/Hero/Hero.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Link } from "react-router-dom";
 import './Hero.css'
 
 const Hero = () => {
@@ -7,13 +7,16 @@ const Hero = () => {
     <div className='hero-img container'>
         <div className='hero-content'>
             <h1 className='heading-primary'>
-                <span>welcome</span> to restaurant
+                <span>welcome</span> to THE ROYAL PARATHA BOX restaurant
             </h1>
 
-            <p className='text-white'>The best gourmet restaurant available in Manhatten</p>
+            <p className='text-white'>The best paratha restaurant available in Electronic City, Bengaluru</p>
 
             <p className='text-white'>
-                Book online or call <span className='special-word'>(555)123-4567</span>
+                Book online or call <span></span>
+                    <Link to="tel:+916366579691" className='special-word'>
+                    (+91)6366-579-691
+                    </Link>  
             </p>
         </div>
     </div>
