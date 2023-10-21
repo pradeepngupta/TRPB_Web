9f89b70f5914c1943417d880ed2f08c90ef90262
Pradeep Gupta <pradeep@zerobill.app>
2023-10-21 09:18:55 +0530

    Added tagline in Hero screen

--- src/pages/Home/Hero/Hero.js
+++ src/pages/Home/Hero/Hero.js
@@ -7,14 +7,15 @@ const Hero = () => {
     <div className='hero-img container'>
         <div className='hero-content'>
             <h1 className='heading-primary'>
-                <span>welcome</span> to THE ROYAL PARATHA BOX restaurant
+                <span>welcome</span> to <br />THE ROYAL PARATHA BOX
             </h1>
-
+            <h3 className='heading-secondary text-white'> Flavours from Home | Ghar ka Swad </h3>
+            <br></br>
             <p className='text-white'>The best paratha restaurant available in Electronic City, Bengaluru</p>
 
             <p className='text-white'>
                 Book online or call <span></span>
-                    <Link to="tel:+916366579691" className='special-word'>
+                    <Link to="tel:+916366579691" className='text-white'>
                     (+91)6366-579-691
                     </Link>  
             </p>
